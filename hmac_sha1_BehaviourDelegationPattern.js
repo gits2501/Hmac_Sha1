ddbb37f6ccf2ee7352ebf89e8dec0812c8ee30ee
gits2501 <gits2501@users.noreply.github.com>
2017-05-23 18:19:48 +0200

    Removed leftover comments

--- hmac_sha1_BehaviourDelegationPattern.js
+++ hmac_sha1_BehaviourDelegationPattern.js
@@ -1,11 +1,3 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
 
 (function () {
     var 
