9377d6af57bdbe2e82b4cf07a6dcd40ecaf340dd
gits2501 <gits2501@users.noreply.github.com>
2017-05-23 18:17:07 +0200

    Added comment about usage

--- hmac_sha1_FunctionalPatern.js
+++ hmac_sha1_FunctionalPatern.js
@@ -505,7 +505,7 @@
 }()); // end Rusha
 
   /////////////////////////////////////////////////////////////////////////////////////////////////
-var HmacSha1 = (function(){ 
+/* var yourHmacSha1 = */  (function(){ 
    "use strict" 
   var sha = new Rusha();   // this is where we make an "instance" of sha1 function
                            // You can use whatever library you choose for sha1,
@@ -659,15 +659,4 @@ var HmacSha1 = (function(){
  return HmacSha1;  // returns function that can be used in constructor calls, with 'new'
 
 })() 
-
-
-var baseStr = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521"
-
-var key = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE";
-
-hmacSha1 = new HmacSha1();
-
-console.log(hmacSha1.digest("ke\\y", "So\\me mess\\age"));
-
-
 
