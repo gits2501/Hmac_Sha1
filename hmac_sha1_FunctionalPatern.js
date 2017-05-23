97068d0282fe740745c45590f84ac9122aaf1c7f
gits2501 <gits2501@users.noreply.github.com>
2017-05-23 17:58:44 +0200

    Added implementation with behavior delegation pattern

--- hmac_sha1_FunctionalPatern.js
+++ hmac_sha1_FunctionalPatern.js
@@ -1,6 +1,7 @@
-
 (function () {
-    var util = {
+    var 
+
+    util = {
         getDataType: function (data) {
             if (typeof data === 'string') {
                 return 'string';
@@ -505,8 +506,8 @@
 }()); // end Rusha
 
   /////////////////////////////////////////////////////////////////////////////////////////////////
-var HmacSha1 = (function(){ 
-   "use strict" 
+(function(){ 
+  "use strict" 
   var sha = new Rusha();   // this is where we make an "instance" of sha1 function
                            // You can use whatever library you choose for sha1,
                            // here we used Rusha.js 
@@ -531,7 +532,8 @@ var HmacSha1 = (function(){
       
        if(kLen > this.blocksize){ 
           key = this.hexToString(sha1(key)); // The hash of 40 hex chars(40bytes) convert to exact char mappings,
-                                           // each char has codepoint from 0x00 to 0xff.Produces string of 20 bytes.
+                                             // each char has codepoint from 0x00 to 0xff. Produces string of
+                                             // 20byte length
          
           var hashedKeyLen =  this.byteLength(key); // take the length of key
        }
@@ -593,7 +595,7 @@ var HmacSha1 = (function(){
       
       for (i; i < len; i++){
         var code = str.charCodeAt(i); // take single character from string
-        if(code >= 0x0 && code <= 0xff) byteLen++;  // check that it is only 1byte in lenght and increase counter
+        if(code >= 0x0 && code <= 0xff) byteLen++; // check that it is only 1byte in lenght and increase counter
         else{
            throw new Error("More the 1 byte code detected, byteLength functon aborted.");
            return;
@@ -659,15 +661,3 @@ var HmacSha1 = (function(){
  return HmacSha1;  // returns function that can be used in constructor calls, with 'new'
 
 })() 
-
-
-var baseStr = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521"
-
-var key = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE";
-
-hmacSha1 = new HmacSha1();
-
-console.log(hmacSha1.digest("ke\\y", "So\\me mess\\age"));
-
-
-
