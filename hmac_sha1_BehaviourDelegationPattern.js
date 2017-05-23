00c40c1ce0a42fc3917649922917e511ef09de97
gits2501 <gits2501@users.noreply.github.com>
2017-05-23 18:06:28 +0200

    Added behaviour delegation pattern and functional pattern

--- hmac_sha1_BehaviourDelegationPattern.js
+++ hmac_sha1_BehaviourDelegationPattern.js
@@ -1,6 +1,16 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
 
 (function () {
-    var util = {
+    var 
+
+    util = {
         getDataType: function (data) {
             if (typeof data === 'string') {
                 return 'string';
@@ -504,96 +514,101 @@
     }
 }()); // end Rusha
 
-  /////////////////////////////////////////////////////////////////////////////////////////////////
-var HmacSha1 = (function(){ 
-   "use strict" 
-  var sha = new Rusha();   // this is where we make an "instance" of sha1 function
-                           // You can use whatever library you choose for sha1,
-                           // here we used Rusha.js 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* var yourHmacSha1 =  */ (function(){ 
+  "use strict" 
+  var sha = new Rusha();  // this is where we make an "instance" of sha1 function
+                          // You can use whatever library you choose for sha1,
+                          // here we used Rusha.js 
   var sha1 = sha.digest;   
   
-  function HmacSha1(){
+  var hmacSha1 = {};
+  hmacSha1.setConstants = function(){  // set constants by puttng them in private object called pocket
+      
+     var pocket = arguments[arguments.length-1] // take last argument
+     pocket.opad = 0x5c;  // outer padding  constant = (0x5c) . And 0x5c is just hexadecimal for backward slash "\"
+     pocket.ipad =  0x36; // outer padding  constant = (0x5c) . And 0x5c is just hexadecimal for backward slash "\"
+                          // We made both constants private. See definition of AttachPocket() function bellow. 
+  }  
+  hmacSha1.digest = function (key, baseString){ // the actual hmac_sha1 function
+     var pocket = arguments[arguments.length-1]; 
+     this.setConstants();
+      
+     var opad_key = "";   // outer padded key
+     var ipad_key = "";   // inner padded key
      this.blocksize = 64; // 64 when using these hash functions: SHA-1, MD5, RIPEMD-128/160 .
-    
-     var opad = 0x5c; // outer padding  constant = (0x5c) . And 0x5c is just hexadecimal for backward slash "\" 
-     var ipad = 0x36; // inner padding contant = (0x36). And 0x36 is hexadecimal for char "6".
-                      // We made both constants private.
      
-     this.digest =  function (key, baseString){ // the actual hmac_sha1 function
-      
-       var opad_key = ""; // outer padded key
-       var ipad_key = ""; // inner padded key
-       var kLen = this.byteLength(key); // length of key in bytes;
+     var kLen = this.byteLength(key); // length of key in bytes;
      
-       if(kLen < this.blocksize){  
-          var diff = this.blocksize - kLen; // diff is how mush  blocksize is bigger then the key
-       }
+     if(kLen < this.blocksize){  
+        var diff = this.blocksize - kLen; // diff is how mush  blocksize is bigger then the key
+     }
       
-       if(kLen > this.blocksize){ 
-          key = this.hexToString(sha1(key)); // The hash of 40 hex chars(40bytes) convert to exact char mappings,
-                                           // each char has codepoint from 0x00 to 0xff.Produces string of 20 bytes.
-         
-          var hashedKeyLen =  this.byteLength(key); // take the length of key
-       }
+     if(kLen > this.blocksize){ 
+        key = this.hexToString(sha1(key)); // The hash of 40 hex chars(40bytes) convert to exact char mappings,
+                                           // each char has codepoint from 0x00 to 0xff. Produces string of
+                                           // 20byte length
+        var hashedKeyLen =  this.byteLength(key); // take the length of key
+     }
       
     
-       (function applyXor(){   // Reads one char, at the time, from key and applies XOR constants on it
-                               // acording to the byteLength of the key.
-         var o_zeroPaddedCode; // result from opading the zero byte
-         var i_zeroPaddedCode; // res from ipading the zero byte
-         var o_paddedCode;     // res from opading the char from key
-         var i_paddedCode;     // res from ipading the char from key
+     (function applyXor(){   // Reads one char, at the time, from key and applies XOR constants on it
+                              // acording to the byteLength of the key.
+        var o_zeroPaddedCode; // result from opading the zero byte
+        var i_zeroPaddedCode; // res from ipading the zero byte
+        var o_paddedCode;     // res from opading the char from key
+        var i_paddedCode;     // res from ipading the char from key
        
-         var char;     // Plaseholder for one char from key 
-         var charCode; // Code, numeric represantation of char 
+        var char;     // Plaseholder for one char from key 
+        var charCode; // Code, numeric represantation of char 
         
-         for(var j = 0; j < this.blocksize; j++){ 
+        for(var j = 0; j < this.blocksize; j++){ 
                
-             if(diff && (j+diff) >= this.blocksize || j >= hashedKeyLen){ // if diff exists (key is shorter then
+           if(diff && (j+diff) >= this.blocksize || j >= hashedKeyLen){ // if diff exists (key is shorter then
                                                             // blocksize) and if we are at boundry where wee should
                                                             // append 0x00 to the lenght of blocksize. Or the key
                                                             // was too lang and was hashed, then also we need to 
                                                             // append 0x00 at the end.
-                o_zeroPaddedCode = 0x00 ^ opad;   
-                opad_key += String.fromCharCode(o_zeroPaddedCode);
+               o_zeroPaddedCode = 0x00 ^ pocket.opad;   
+               opad_key += String.fromCharCode(o_zeroPaddedCode);
                  
-                i_zeroPaddedCode = 0x00 ^ ipad;
-                ipad_key += String.fromCharCode(i_zeroPaddedCode);
-              }
-              else {
-                char = this.oneByteCharAt(key,j);     // take char from key, only one byte char
-                 charCode = char.codePointAt(0); // convert that char to number
+               i_zeroPaddedCode = 0x00 ^ pocket.ipad;
+               ipad_key += String.fromCharCode(i_zeroPaddedCode);
+           }
+           else {
+               char = this.oneByteCharAt(key,j);  // take char from key, only one byte char
+               charCode = char.codePointAt(0);    // convert that char to number
                   
-                 o_paddedCode =  charCode ^ opad; // XOR the char code with outer padding constant (opad)
-                 opad_key += String.fromCharCode(o_paddedCode); // convert back code result to string
+               o_paddedCode =  charCode ^ pocket.opad; // XOR the char code with outer padding constant (opad)
+               opad_key += String.fromCharCode(o_paddedCode); // convert back code result to string
                   
-                 i_paddedCode = charCode ^ ipad;  // XOR with the inner padding constant (ipad)
-                 ipad_key += String.fromCharCode(i_paddedCode);
+               i_paddedCode = charCode ^ pocket.ipad;  // XOR with the inner padding constant (ipad)
+               ipad_key += String.fromCharCode(i_paddedCode);
                
-              }
+           }
             
   
               
-          }
-       //   console.log("opad_key: ", "|"+opad_key+"|", "\nipad_key: ", "|"+ipad_key+"|"); // Prints opad and
-                                                                                // ipad key, line can be deleted.
-       }.bind(this))() // binding "this" reference in applyXor to each "instance" of HmacSha1  
-   
-         return sha1(opad_key + this.hexToString(sha1(ipad_key + baseString))) ;
+        }
+       // console.log("opad_key: ", "|"+opad_key+"|", "\nipad_key: ", "|"+ipad_key+"|"); // Prints opad and
+                                                                              // ipad key, line can be deleted.
+     }.bind(this))() // binding "this" reference in applyXor to each "instance" of HmacSha1  
+    return sha1(opad_key + this.hexToString(sha1(ipad_key + baseString))) ;
       
-     }
   }
+
   
-  HmacSha1.prototype.byteLength  = function (str){  // counts characters only 1byte in length, of a string.
-                                                    // Very similar to oneByteChar()
-                                                    // For clarity 2 funtions are made.
+  hmacSha1.byteLength  = function (str){  // counts characters only 1byte in length, of a string.
+                                          // Very similar to oneByteChar().
+                                          // For clarity 2 funtions are made.
       var len = str.length;
       var i = 0;
       var byteLen = 0; // string byte lenght
       
       for (i; i < len; i++){
         var code = str.charCodeAt(i); // take single character from string
-        if(code >= 0x0 && code <= 0xff) byteLen++;  // check that it is only 1byte in lenght and increase counter
+        if(code >= 0x0 && code <= 0xff) byteLen++; // check that it is only 1byte in lenght and increase counter
         else{
            throw new Error("More the 1 byte code detected, byteLength functon aborted.");
            return;
@@ -605,10 +620,10 @@ var HmacSha1 = (function(){
     
   }
   
-  HmacSha1.prototype.oneByteCharAt = function (str,idx){
+  hmacSha1.oneByteCharAt = function (str,idx){
        var code = str.codePointAt(idx);
        if(code >= 0x00 && code <= 0xff){ // we are interested at reading only one byte
-          return str.charAt(idx); // return char.
+          return str.charAt(idx); // return chars
        }    
        else{ 
           throw new Error("More then 1byte character detected, |oneByteCharAt()| function  is aborted.")
@@ -616,22 +631,22 @@ var HmacSha1 = (function(){
     
   }
   
-  HmacSha1.prototype.hexToString = function (sha1Output){ // Converts every pair of hex CHARS to their character
-                                                          // conterparts.
-                                                          // example1: "4e" is converted to char "N" 
-                                                          // example2: "34" is converted to char "4"
+  hmacSha1.hexToString = function (sha1Output){ // Converts every pair of hex CHARS to their character
+                                                // conterparts.
+                                                // example1: "4e" is converted to char "N" 
+                                                // example2: "34" is converted to char "4"
       
-    var l;        // char at "i" place, left
-    var lcode;    // code parsed from left char
-    var shiftedL; // left character shifted to the left 
+   var l;        // char at "i" place, left
+   var lcode;    // code parsed from left char
+   var shiftedL; // left character shifted to the left 
       
-    var r;        // char at "i+1" place, right
-    var rcode;    // code parsed from right char
+   var r;        // char at "i+1" place, right
+   var rcode;    // code parsed from right char
     
-    var bin;      // code from bitwise OR operation
-    var char;     // one character
-    var result = ""; // result string 
-      
+   var bin;      // code from bitwise OR operation
+   var char;     // one character
+   var result = ""; // result string 
+     
    for (var i = 0; i < sha1Output.length; i+=2){ // in steps by 2
            l = sha1Output[i]; // take "left" char
            
@@ -653,21 +668,32 @@ var HmacSha1 = (function(){
     }
     // console.log("|"+result+"|", result.length); // prints info, line can be deleted
      
-    return result;
+   return result;
   }
 
- return HmacSha1;  // returns function that can be used in constructor calls, with 'new'
+ function AttachPocket (){ // fucntion that attaches private object (pocket) to new "instance" of hmacSha1 and returns that instance
+    
+     var pocket = {}; 
+     
+     var publicAPI = Object.create(hmacSha1); //  making new "instance" of hmacSha1 , linking prototype chains.
+     publicAPI.digest = function(){
+         
+         var args = Array.prototype.slice.call(arguments);
+         args = args.concat([pocket]); // add pocket as last argument
+         return hmacSha1.digest.apply(this, args)
+            
+     }
+     publicAPI.setConstants = function(){
+         var args = Array.prototype.slice.call(arguments);
+         args = args.concat([pocket]); // add pocket as last argument
+         
+         return hmacSha1.setConstants.apply(this, args)
+     }    
+         
+    return publicAPI;    
+  } 
+     
+   
+ return AttachPocket;  // returns function that can be used with new but really doesnt have to, since it returns own object
 
-})() 
-
-
-var baseStr = "POST&https%3A%2F%2Fapi.twitter.com%2F1%2Fstatuses%2Fupdate.json&include_entities%3Dtrue%26oauth_consumer_key%3Dxvz1evFS4wEEPTGEFPHBog%26oauth_nonce%3DkYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1318622958%26oauth_token%3D370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb%26oauth_version%3D1.0%26status%3DHello%2520Ladies%2520%252B%2520Gentlemen%252C%2520a%2520signed%2520OAuth%2520request%2521"
-
-var key = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw&LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE";
-
-hmacSha1 = new HmacSha1();
-
-console.log(hmacSha1.digest("ke\\y", "So\\me mess\\age"));
-
-
-
+})()
